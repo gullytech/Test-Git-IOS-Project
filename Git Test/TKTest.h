b99d7f1533844f8eebe301458e6ac27e6bd57228
Tim <tim@Tims-MacBook-Pro.local>
2014-05-08 08:05:46 +1000

    added a test class

---
+++ Git Test/TKTest.h
@@ -0,0 +1,13 @@
+//
+//  TKTest.h
+//  Git Test
+//
+//  Created by Tim on 8/05/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKTest : NSObject
+
+@end
---
+++ Git Test/TKTest.m
@@ -0,0 +1,13 @@
+//
+//  TKTest.m
+//  Git Test
+//
+//  Created by Tim on 8/05/2014.
+//
+//
+
+#import "TKTest.h"
+
+@implementation TKTest
+
+@end
